a7edef86446afe0f29d3247a2aba4eca63948374
Simon Hausmann <hausmann@gmail.com>
2024-07-31 15:19:49 +0200

    Enable D-Cache on the h747i

--- stm32h747i-disco/CM7/Core/Src/main.c
+++ stm32h747i-disco/CM7/Core/Src/main.c
@@ -81,6 +81,9 @@ int main(void)
   /* Enable I-Cache---------------------------------------------------------*/
   SCB_EnableICache();
 
+  /* Enable D-Cache---------------------------------------------------------*/
+  SCB_EnableDCache();
+
 /* USER CODE BEGIN Boot_Mode_Sequence_1 */
   /* Wait until CPU2 boots and enters in stop mode or timeout*/
   /*
