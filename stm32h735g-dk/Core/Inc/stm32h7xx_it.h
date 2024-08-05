c4a607317fa8b1b5c7fc5b94d7b762d9322a5970
Simon Hausmann <hausmann@gmail.com>
2024-08-05 11:43:37 +0200

    stm32h735g-dk: fix build

--- stm32h735g-dk/Core/Inc/stm32h7xx_it.h
+++ stm32h735g-dk/Core/Inc/stm32h7xx_it.h
@@ -56,7 +56,6 @@ void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
 void TIM6_DAC_IRQHandler(void);
-void LTDC_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
--- stm32h735g-dk/Core/Src/stm32h7xx_it.c
+++ stm32h735g-dk/Core/Src/stm32h7xx_it.c
@@ -213,20 +213,6 @@ void TIM6_DAC_IRQHandler(void)
   /* USER CODE END TIM6_DAC_IRQn 1 */
 }
 
-/**
-  * @brief This function handles LTDC global interrupt.
-  */
-void LTDC_IRQHandler(void)
-{
-  /* USER CODE BEGIN LTDC_IRQn 0 */
-
-  /* USER CODE END LTDC_IRQn 0 */
-  HAL_LTDC_IRQHandler(&hltdc);
-  /* USER CODE BEGIN LTDC_IRQn 1 */
-
-  /* USER CODE END LTDC_IRQn 1 */
-}
-
 /* USER CODE BEGIN 1 */
 
 /* USER CODE END 1 */
